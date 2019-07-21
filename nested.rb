82bbffed38367628227cb36bc54078aa5e6fed48
Emily G <aspentree79@gmail.com>
2019-07-21 12:34:25 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -76,7 +76,7 @@ def adding_matz
         }
      }
 
-		 add_yokihiro = {:yokihiro_matsumoto => :known_for}
+		 programmer_hash << [:yokihiro_matsumoto]
 		 puts programmer_hash
 
 	# 	 programmer_hash[:yokihiro_matsumoto] = [:known_for]
