e44e83cc7d9b2c8dd611f2f405e0a81b1a25728a
Emily G <aspentree79@gmail.com>
2019-07-21 13:23:53 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -75,13 +75,7 @@ def adding_matz
           :languages => "C"
         }
      }
-
-		#  programmer_hash[:yukihiro_matsumoto] =  { [
-		#  :known_for => "Ruby",
-		#   :languages => "LISP", "C" ] }
-		# puts programmer_hash
 programmer_hash[:yukihiro_matsumoto] = {}
-# programmer_hash[:yukihiro_matsumoto][:known_for]
 		 programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
 		programmer_hash[:yukihiro_matsumoto][:known_for][:languages] = ["LISP", "C"]
 		 return programmer_hash
