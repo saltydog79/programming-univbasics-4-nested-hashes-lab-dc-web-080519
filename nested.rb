31a00ebe761d883785a706a4db7bd4098c49a1dd
Emily G <aspentree79@gmail.com>
2019-07-21 13:42:45 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -60,26 +60,6 @@ end
 
 
 def adding_matz
-	# programmer_hash =
- # 		{
-  #       :grace_hopper => {
-  #         :known_for => "COBOL",
-  #         :languages => ["COBOL", "FORTRAN"]
-  #       },
-  #       :alan_kay => {
-  #         :known_for => "Object Orientation",
-  #         :languages => ["Smalltalk", "LISP"]
-  #       },
-  #       :dennis_ritchie => {
-  #         :known_for => "Unix",
-  #         :languages => ["C"]
-  #       }
-  #    }
-	# 	 programmer_hash[:yukihiro_matsumoto] = {}
-	# 	 programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
-	# 	 programmer_hash[:yukihiro_matsumoto][:known_for][:languages] = ["LISP", "C"]
-	# 	 return programmer_hash
-
 	programmer_hash =
 		 {
 		:grace_hopper => {
